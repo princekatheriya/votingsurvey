b8ea0def699c9e95529e0b96687e08381003adea
Prince Katheriya <princekatheriya2@gmail.com>
2021-04-13 11:58:37 +0530

    validation

--- src/App.js
+++ src/App.js
@@ -21,9 +21,10 @@ class App extends Component {
       guides: null,
       newData: "",
       vil: "",
-      radio:'',
+      radio: "",
       currentUser: {},
       userImages: null,
+      checkUser: null,
     };
 
     this.userRef = database.ref("/users").child("Anonymous");
@@ -41,8 +42,9 @@ class App extends Component {
   componentDidMount() {
     auth.onAuthStateChanged((currentUser) => {
       this.setState({ currentUser: currentUser || {} });
-
-      if (currentUser) {
+      this.setState({ checkUser: currentUser });
+      console.log("c :- ", this.state.checkUser);
+      if (this.state.currentUser) {
         // Init current user Refs
         this.userRef = database.ref("/users").child(currentUser.uid);
         this.userStorageRef = storage.ref("/user-files").child(currentUser.uid);
@@ -95,10 +97,14 @@ class App extends Component {
     console.log("data:- ", this.state);
     this.guidesRef.push({
       uid: currentUser.uid,
+      name: currentUser.displayName,
       // content: newData,
       vil: vil,
-      radio:radio
+      radio: radio,
     });
+    setTimeout(function () {
+      alert("Thank you for participating");
+    }, 3000);
   }
 
   handleFileSubmit(event) {
@@ -148,17 +154,16 @@ class App extends Component {
       ));
     }
   }
-  
 
   render() {
-    const car = require('./assets/images/car.jpeg');
-    const imli = require('./assets/images/imli.jpeg');
-    const camera = require('./assets/images/camera.png');
-    const carrom = require('./assets/images/carrom.jpeg');
-    const kitab = require('./assets/images/kitab.png');
-    const anaj = require('./assets/images/kisaan.jpeg');
-    const kanni = require('./assets/images/kanni.jpeg')
-    const coat = require('./assets/images/coat.png')
+    const car = require("./assets/images/car.jpeg");
+    const imli = require("./assets/images/imli.jpeg");
+    const camera = require("./assets/images/camera.png");
+    const carrom = require("./assets/images/carrom.jpeg");
+    const kitab = require("./assets/images/kitab.png");
+    const anaj = require("./assets/images/kisaan.jpeg");
+    const kanni = require("./assets/images/kanni.jpeg");
+    const coat = require("./assets/images/coat.png");
     return (
       <div className="App">
         <div className="App-nav">
@@ -184,95 +189,191 @@ class App extends Component {
             <b>मतदान सर्वेक्षण / Voting Survey</b>
           </code>
         </p>
-        <div className="AppBody">
-          <form className="App-form" onSubmit={this.handleSubmit}>
-            <label for="cars">एक गाँव चुनें / Choose a Village:</label>
-            <select name="cars" id="cars" onChange={this.handleChangeOption} required>
-            <option value="">कृप्या चुनें / None</option>
-              <option value="kasumara">कसुमरा-टहा / Kasumara-Taha</option>
-              <option value="manona">मनोना / Manona</option>
-              <option value="others">अन्य / others</option>
-            </select>
-            {/* <input
+        {this.state.checkUser === null ? (
+          <div style={{ textAlign: "center" }}>
+            <code>
+              <div className="AppBody-fb-db1">
+                <h2 style={{ color: "Red" }}>
+                  कृपया पहले साइन इन करें / Please Sign In First{" "}
+                </h2>
+              </div>
+            </code>
+          </div>
+        ) : null}
+        {this.state.guides === null ? (
+          <div className="AppBody">
+            <form className="App-form" onSubmit={this.handleSubmit}>
+              <label for="cars">एक गाँव चुनें / Choose a Village:</label>
+              <select
+                name="cars"
+                id="cars"
+                onChange={this.handleChangeOption}
+                required
+              >
+                <option value="">कृप्या चुनें / None</option>
+                <option value="kasumara">कसुमरा-टहा / Kasumara-Taha</option>
+                <option value="manona">मनोना / Manona</option>
+                <option value="others">अन्य / others</option>
+              </select>
+              {/* <input
               className="text"
               name="name"
               placeholder="New data"
               type="text"
               onChange={this.handleChange}
             /> */}
-              
 
+              <p>कृपया अपना विकल्प चुनें / Please select your option:-</p>
+              <div style={{}} onChange={this.handleChangeRadio}>
+                <div style={{ display: "inline" }}>
+                  <div
+                    style={{
+                      display: "inline-flex",
+                      marginTop: 20,
+                      marginBottom: 20,
+                    }}
+                  >
+                    <span style={{ margin: 20 }}>1. </span>{" "}
+                    <img src={anaj} width="100" height="50" />
+                  </div>
+                  <div style={{ display: "inline" }}>
+                    <input type="radio" value="ANAJ" name="gender" required />{" "}
+                  </div>
+                </div>
 
-            <p>कृपया अपना विकल्प चुनें / Please select your option:-</p>
-            <div style={{}} onChange={this.handleChangeRadio} >
-        <div style={{display:'inline'}}>
-        <div style={{display:'inline-flex',marginTop:20, marginBottom:20}}>
-            <span style={{margin:20}}>1.   </span> <img src={anaj} width="100" height="50" />
-          </div>
-          <div style={{display:'inline',}}><input type="radio" value="ANAJ" name="gender" required/> </div>
-          
-        </div>
+                <div style={{}}>
+                  <div
+                    style={{
+                      display: "inline-flex",
+                      marginTop: 20,
+                      marginBottom: 20,
+                    }}
+                  >
+                    <span style={{ margin: 20 }}>2.</span>
+                    <img src={imli} width="100" height="50" />
+                  </div>
+                  <div style={{ display: "inline" }}>
+                    <input type="radio" value="IMLI" name="gender" required />{" "}
+                  </div>
+                </div>
 
-        <div style={{}}>
-        <div style={{display:'inline-flex', marginTop:20, marginBottom:20}}>
-        <span style={{margin:20}}>2.</span><img src={imli} width="100" height="50" />
-          </div>
-          <div style={{display:'inline'}}><input type="radio" value="IMLI" name="gender" required/> </div>
-          </div>
+                <div style={{}}>
+                  <div
+                    style={{
+                      display: "inline-flex",
+                      marginTop: 20,
+                      marginBottom: 20,
+                    }}
+                  >
+                    <span style={{ margin: 20 }}>3. </span>{" "}
+                    <img src={kanni} width="100" height="50" />
+                  </div>
+                  <div style={{ display: "inline" }}>
+                    <input type="radio" value="KANNI" name="gender" required />{" "}
+                  </div>
+                </div>
 
-          <div style={{}}>
-        <div style={{display:'inline-flex', marginTop:20, marginBottom:20}}>
-        <span style={{margin:20}}>3. </span> <img src={kanni} width="100" height="50" />
-          </div>
-          <div style={{display:'inline'}}><input type="radio" value="KANNI" name="gender" required/> </div>
-          </div>
+                <div style={{}}>
+                  <div
+                    style={{
+                      display: "inline-flex",
+                      marginTop: 20,
+                      marginBottom: 20,
+                    }}
+                  >
+                    <span style={{ margin: 20 }}>4. </span>
+                    <img src={car} width="100" height="50" />
+                  </div>
+                  <div style={{ display: "inline" }}>
+                    <input type="radio" value="CAR  " name="gender" required />{" "}
+                  </div>
+                </div>
 
-          <div style={{}}>
-        <div style={{display:'inline-flex', marginTop:20, marginBottom:20}}>
-        <span style={{margin:20}}>4.   </span><img src={car} width="100" height="50" />
-          </div>
-          <div style={{display:'inline'}}><input type="radio" value="CAR  " name="gender" required/> </div>
-          </div>
+                <div style={{}}>
+                  <div
+                    style={{
+                      display: "inline-flex",
+                      marginTop: 20,
+                      marginBottom: 20,
+                    }}
+                  >
+                    <span style={{ margin: 20 }}>5. </span>{" "}
+                    <img src={kitab} width="100" height="50" />
+                  </div>
+                  <div style={{ display: "inline" }}>
+                    <input type="radio" value="KITAB" name="gender" required />{" "}
+                  </div>
+                </div>
 
-          <div style={{}}>
-        <div style={{display:'inline-flex', marginTop:20, marginBottom:20}}>
-        <span style={{margin:20}}>5.   </span> <img src={kitab} width="100" height="50" />
-          </div>
-          <div style={{display:'inline'}}><input type="radio" value="KITAB" name="gender" required/> </div>
-          </div>
+                <div style={{}}>
+                  <div style={{ display: "inline-flex" }}>
+                    <span style={{ margin: 20 }}>6. </span>{" "}
+                    <img src={camera} width="100" height="50" />
+                  </div>
+                  <div style={{ display: "inline" }}>
+                    <input type="radio" value="CAMERA" name="gender" required />{" "}
+                  </div>
+                </div>
 
-          <div style={{}}>
-        <div style={{display:'inline-flex'}}>
-        <span style={{margin:20}}>6.   </span> <img src={camera} width="100" height="50" />
-          </div>
-          <div style={{display:'inline'}}><input type="radio" value="CAMERA" name="gender" required/> </div>
-          </div>
+                <div style={{}}>
+                  <div
+                    style={{
+                      display: "inline-flex",
+                      marginTop: 20,
+                      marginBottom: 20,
+                    }}
+                  >
+                    <span style={{ margin: 20 }}>7. </span>
+                    <img src={carrom} width="100" height="50" />
+                  </div>
+                  <div style={{ display: "inline" }}>
+                    <input type="radio" value="CARROM" name="gender" required />{" "}
+                  </div>
+                </div>
 
-          <div style={{}}>
-        <div style={{display:'inline-flex', marginTop:20, marginBottom:20}}>
-        <span style={{margin:20}}>7.   </span><img src={carrom} width="100" height="50" />
-          </div>
-          <div style={{display:'inline'}}><input type="radio" value="CARROM" name="gender" required/> </div>
-          </div>
+                <div style={{}}>
+                  <div
+                    style={{
+                      display: "inline-flex",
+                      marginTop: 20,
+                      marginBottom: 20,
+                    }}
+                  >
+                    <span style={{ margin: 20 }}>8. </span>{" "}
+                    <img style={{}} src={coat} width="100" height="50" />
+                  </div>
+                  <div style={{ display: "inline" }}>
+                    <input type="radio" value="COAT" name="gender" required />
+                  </div>
+                </div>
+              </div>
 
-          <div style={{}}>
-        <div style={{display:'inline-flex', marginTop:20, marginBottom:20}}>
-        <span style={{margin:20}}>8.   </span> <img style={{}} src={coat} width="100" height="50" />
-          </div>
-          <div style={{display:'inline'}}>
-            <input type="radio" value="COAT" name="gender" required/>
-          </div>
-          </div>
-
-      </div>
-
-
-      <input className="button" type="submit" value="Submit" />
-          </form>
-          {/* <pre className="AppBody-fb-db">
+              {this.state.checkUser === null ? (
+                <div style={{ marginBottom: 20 }}>
+                  <input
+                    className="button1"
+                    type="submit"
+                    value="Submit"
+                    disabled
+                  />
+                </div>
+              ) : (
+                <div style={{ marginBottom: 20 }}>
+                  <input className="button" type="submit" value="Submit" />
+                </div>
+              )}
+            </form>
+            {/* <pre className="AppBody-fb-db">
             {JSON.stringify(this.state.guides, null, 2)}
           </pre> */}
-        </div>
+          </div>
+        ) : (
+          <pre className="AppBody-fb-db">
+            <p style={{ color: "green" }}>
+              Thank You for taking part in the Survey
+            </p>
+          </pre>
+        )}
 
         {/* <p className="App-intro">
           <code>
